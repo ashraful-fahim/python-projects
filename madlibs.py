2db1b8b8d1246bfe0fa3caf1d127796be2abe928
ashraful-fahim <ashraful30june@gmail.com>
2025-07-21 12:08:44 +0600

    created another madlib game and tested it.

--- madlibs.py
+++ madlibs.py
@@ -1,6 +1,17 @@
 # string concatenation (aka how to put strings together)
 # suppose we want to create a string that says "Subscribe to _____ "
 
-youtuber = ""
 
-print(f"subscribe to {youtuber}")
+# youtuber = ""
+# print(f"subscribe to {youtuber}")
+
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
